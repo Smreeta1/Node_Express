be2a172ffaef30f4a82667c446379af12c727cc7
Smreeta Shrestha <smreetashrestha1@gmail.com>
2023-11-03 13:37:21 +0545

    create,read and update completed

---
+++ routes/books.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var books =require('../Resources/books')
+
+
+/* GET home page. */
+router.get('/add', function(req, res, next) {
+  res.render('addbooks', { title: 'Addbooks'});//displays
+});
+router.post('/save',function(req,res,next){
+    books.push({...req.body,_id:`00${books.length +1}`})
+    res.redirect('/');
+})
+
+    // router.get('/edit/:_id',function(req, res, next){
+     
+    //     const book =books.find((book)=>{
+    //         book._id===req.params._id
+    //         res.render('editbooks', { title:"edit books",book})
+
+    //     })
+    // })
+
+    router.get('/edit/:_id', function(req, res, next){
+        const book = books.find((book)=>book._id === req.params._id)
+        res.render('editBooks', {title: "Edit Books", book})
+      })
+      
+    
+      
+      
+      
+      
+    router.post('/saveEdited/:_id', function(req, res, next){
+        const currIndex = books.findIndex((book)=>book._id === req.params._id)
+        books.splice(currIndex, 1, {...req.body, _id: req.params._id})
+        res.redirect('/')
+      })
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var books=require('../Resources/books');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BookApp' });
+  res.render('index', { title: 'BookApp',bookList: books}); //index.ejs
 });
 
 module.exports = router;
